138ab55ddffe8bc82319f72b3f05cbd44de233a0
Martin Gleich <martin.gleich2@googlemail.com>
2025-03-05 18:26:47 +0100

    Add tests

--- Tests/Calculator.cs
+++ Tests/Calculator.cs
@@ -1,12 +1,15 @@
-﻿namespace ParserCombinator;
+﻿using ParserCombinator;
+
+namespace Tests;
+
 using static Parser;
 
-public static class Program
+public class Calculator
 {
     private static int ParseInt(IEnumerable<char> digits) => digits.Aggregate(0, (accum, d) => accum * 10 + (d - '0'));
     private static Parser<Func<int, int, int>> Op(char op, Func<int, int, int> f) => Char(op).Return(f);
 
-    public static void Main()
+    private static readonly Parser<int> Expression = Create(() =>
     {
         var number = Digit.Some().Select(ParseInt);
         var opSum = OneOf(
@@ -16,7 +19,8 @@ public static class Program
             Op('*', (a, b) => a * b),
             Op('/', (a, b) => a / b));
         var opNegate = Char('-').Skip(Whitespaces).Any().Select(x => x.Count() % 2 == 1);
-        var expr = Recursive<int>(expr =>
+
+        return Recursive<int>(expr =>
         {
             var bracketedExpr = Surround(Char('(').Skip(Whitespaces), expr, Char(')'));
             var term = OneOf(number, bracketedExpr);
@@ -24,8 +28,22 @@ public static class Program
             var product = ChainLeft(negation, opProduct.Skip(Whitespaces));
             return ChainLeft(product, opSum.Skip(Whitespaces));
         });
-        Console.WriteLine(expr("1+2* -3"));
-        Console.WriteLine(expr("2*( 1 - 2)"));
-        Console.WriteLine(expr("- -1"));
-    }
+    });
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("456", 456)]
+    [InlineData("-2", -2)]
+    [InlineData("1+2* -3", -5)]
+    [InlineData("2*( 1 - 2)", -2)]
+    [InlineData("- -1", 1)]
+    public void Good(string text, int value) => Assert.Equal(value, Expression(text).Value);
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("-(1 - 2")]
+    [InlineData("1+2*")]
+    [InlineData("2*")]
+    [InlineData("-")]
+    public void Fail(string text) => Assert.False(Expression(text).IsOkay);
 }
