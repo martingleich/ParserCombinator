1e54227aa2daee3fecd4d1233f6ef372f5dab0af
Martin Gleich <m.gleich@codesys.com>
2025-03-06 08:41:31 +0100

    Cleanup prefix operators

---
+++ Tests/Functional.cs
@@ -0,0 +1,6 @@
+﻿namespace Tests;
+
+public static class Functional
+{
+    public static Func<T, T> Flatten<T>(this IEnumerable<Func<T, T>> funcs) => x => funcs.Aggregate(x, (acc, func) => func(acc));
+}
